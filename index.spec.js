229b30262d1f42734ca66b9b9e6bf1e98a8c1ef3
Mol <mol360@gmail.com>
2021-06-27 11:24:42 -0300

    * Add BillFormat class tests.

---
+++ index.spec.js
@@ -0,0 +1,55 @@
+const BarcodeValidation = require('./Barcode/BarcodeValidation');
+
+
+describe('Module Calcule Validation', () => {
+	it('A valid ten module calculation', () => {
+		const barcode = "21290001192110001210904475617405975870000002000";
+		var isValid = true;
+		const validStr = "0447561740";
+		const validDigitVerification = "5";
+		var barcodeBillFormatValidation;
+		
+		try {
+			barcodeBillFormatValidation = new BarcodeValidation(barcode);
+		} catch (e) {
+			//
+		}
+
+		isValid = barcodeBillFormatValidation.isValidTenModule(validStr, validDigitVerification);
+
+		expect(isValid).toBe(true);
+	});
+});
+
+describe('Bill Format Test', () => {
+	const validBillFormatBarcode = "21290001192110001210904475617405975870000002000";
+	var barcodeBillFormatValidation;
+
+	it('A valid BillFormat barcode format', () => {
+		var isValid = true;
+
+		try {
+			barcodeBillFormatValidation = new BarcodeValidation(validBillFormatBarcode);
+		} catch (e) {
+			isValid = false;
+			console.log("Error : " + e);
+		}
+
+		expect(isValid).toBe(true);
+	});
+
+	it('A valid BillFormat second block with verification digit', () => {
+		var isValid = barcodeBillFormatValidation.isBarcodeSecondBlockValid();
+		expect(isValid).toBe(true);
+	});
+
+	it('A valid BillFormat third block with verification digit', () => {
+		var isValid = barcodeBillFormatValidation.isBarcodeThirdBlockValid();
+		expect(isValid).toBe(true);
+	});
+
+	it('A valid BillFormat fourth block with verification digit', () => {
+		var isValid = barcodeBillFormatValidation.isBarcodeFourthBlockValid();
+		expect(isValid).toBe(true);
+	});
+});
